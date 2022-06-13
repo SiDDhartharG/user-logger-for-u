d0cad560ae016ec1f22f09bb1c2647b38817316c
Siddharth Garg <garg.sid6665@gmail.com>
2022-06-13 22:32:05 +0530

    update package.json and debug directory

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve()
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/user_log_frontend/build')))
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'user_log_frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
