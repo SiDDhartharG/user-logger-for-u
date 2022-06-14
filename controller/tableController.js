41b2c9ab5b042d0510b0bc2f8f9596cb7019378e
Siddharth Garg <garg.sid6665@gmail.com>
2022-06-14 10:55:53 +0530

    add delete table api

--- controller/tableController.js
+++ controller/tableController.js
@@ -1,4 +1,6 @@
+import Entitie from "../models/entityModels.js"
 import User from "../models/userModels.js"
+import { removeItemOnce } from "../utils/array.js"
 import generateToken from "../utils/token.js"
 
 //@route POST /api/table/:tableName
@@ -21,8 +23,23 @@ const addTable = async (req, res) => {
 //@route DELETE /api/table/:tableName
 //@desc middleware
 const deleteTable = async (req, res) => {
-    // delete all entities 
-    // pop from user.tableName
+    const tableNameToDelete = req.params.tableName
+    if (!tableNameToDelete || tableNameToDelete === '') {
+        return res.status(400).json({ message: "Wrong/Empty Table Name" })
+    }
+    var { _id, email, name, tableName = [] } = req.user
+    try {
+        // delete all entity from table
+        await Entitie.deleteMany({ tableName: tableNameToDelete, userId: _id })
+
+        // remove table from table array
+        await User.findOneAndUpdate({ _id }, { $pull: { tableName: tableNameToDelete } })
+
+        tableName = removeItemOnce(tableName, tableNameToDelete)
+        res.status(201).json({ token: generateToken(_id, name, email, tableName) })
+    } catch (error) {
+        res.status(501).json({ error: error.toString() })
+    }
 }
 
 export {
